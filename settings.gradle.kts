7402081b3e39d7154e8bf5fa536bd7fa0383b8c0
Leandro Otoni <leandro.otonii@gmail.com>
2024-04-24 13:44:30 -0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    includeBuild("convention-plugins")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kmp-lib-poc"
+include(":library")
